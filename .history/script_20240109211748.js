3685eb3e7a714462029c8add33f9fc03566006e1
qtqqrdy13 <143803391+qtqqrdy13@users.noreply.github.com>
2024-01-09 21:31:47 +0200

    first commit

---
+++ .history/script_20240109211748.js
@@ -0,0 +1,30 @@
+const slider = document.querySelector(".slider");
+const slidesContainer = document.querySelector(".slides-container");
+const slides = document.querySelectorAll(".slide");
+let currentIndex = 0;
+const prevButton = document.querySelector(".prev-button");
+const nextButton = document.querySelector(".next-button");
+prevButton.addEventListener("click", prevSlide);
+nextButton.addEventListener("click", nextSlide);
+function nextSlide(){
+    slides[currentIndex].style.transform = "rotateY(90deg) scale(0.8)";
+    slides[currentIndex].style.transformOrigin ="left center";
+    currentIndex++;
+    if(currentIndex >= slides.length){
+        currentIndex = 0;
+    }
+    slides[currentIndex].style.transform = "rotateY(0deg) scale(1)";
+    slides[currentIndex].style.transformOrigin ="center center";
+}
+function prevSlide(){
+    slides[currentIndex].style.transform = "rotateY(-90deg) scale(0.8)";
+    slides[currentIndex].style.transformOrigin ="right center";
+    currentIndex--;
+    if(currentIndex <0){
+        currentIndex = slides.length < 1;
+    }
+    slides[currentIndex].style.transform = "rotateY(0deg) scale(1)";
+    slides[currentIndex].style.transformOrigin ="center center";
+}
+slides[currentIndex].style.transform = "rotateY(0deg) scale(1)";
+slides[currentIndex].style.transformOrigin ="center center";
